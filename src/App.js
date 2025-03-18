f730edc6c0b0c8996a57b967fc0ba11da2e30be4
Yeswanth Sai Tirumalasetty <111565244+yeswanthsai18@users.noreply.github.com>
2025-03-18 03:11:40 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
